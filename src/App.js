f4b62b63e98d5737ade87eef4484abb5d672cf09
rociodesantiago <rocio.codemonster@gmail.com>
2019-02-16 15:51:58 -0500

    add blank pages - assign pages to team members through issues

--- src/App.js
+++ src/App.js
@@ -1,27 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+// import logo from './logo.svg';
 import './App.css';
+import Kingdom from './pages/Kingdom'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={Kingdom}/>
+          </Switch>
+        </BrowserRouter>
       </div>
-    );
+    )
   }
 }
 
